512c627e6c7cff34f202cbffb82a59561a53179c
Shane Oh <ohshane71@gmail.com>
2021-02-23 13:27:10 +0900

    added cors for post req

--- posts/index.js
+++ posts/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // const bodyParser = require('express-json');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(cors());
 
 const posts = {};
 
